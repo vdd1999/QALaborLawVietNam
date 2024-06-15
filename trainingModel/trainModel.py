63cb16b02fe6ddff3e240d686746e41cec5ff466
Vu Dat <dinhdat187199@gmail.com>
2024-06-15 16:45:52 +0700

    add model word2vec

--- trainingModel/trainModel.py
+++ trainingModel/trainModel.py
@@ -1,8 +1,3 @@
-from rank_bm25 import BM25Okapi
-
-# Khởi tạo BM25 với các đoạn văn bản đã được token hóa
-bm25 = BM25Okapi([context.split() for context in tokenized_contexts])
-
 # WORD 2 VEC
 from gensim.models import Word2Vec
 
@@ -11,4 +6,4 @@ tokenized_corpus = [context.split() for context in tokenized_contexts]
 word2vec_model = Word2Vec(sentences=tokenized_corpus, vector_size=100, window=5, min_count=1, workers=4)
 
 # Lưu mô hình Word2Vec đã huấn luyện
-word2vec_model.save("word2vec.model")
+# word2vec_model.save("word2vec.model")
