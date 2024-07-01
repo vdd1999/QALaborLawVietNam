d5f2581a6711dfa9e5bc5b111dea0dd858d0eca1
daocaman <daocaman1998@gmail.com>
2024-07-01 15:43:48 +0700

    update installing dependencies

--- install.bat
+++ install.bat
@@ -1,6 +1,5 @@
-!py -V:3.11 -m pip install underthesea
-!py -V:3.11 -m pip install gensim
-!py -V:3.11 -m pip install scipy==1.10.1
-!py -V:3.11 -m pip install flask
-
-.\venv\Scripts\Activate.ps1
+pip install underthesea
+pip install gensim
+pip install scipy==1.10.1
+pip install flask
+pip install rank_bm25
